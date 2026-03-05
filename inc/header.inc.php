f8e713ecacd82dac23a1527ef5cf57246eb2c8df
odysseyboy <ruanmachadodonati9@gmail.com>
2026-03-05 20:25:42 -0300

    menu responsivo(mais ou menos)

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -9,18 +9,34 @@
 </head>
 <body>
     <header>
-        <a href="index.php"><img src ="img/orbitaicon.png"></a>
-        <nav class = "menu_opcoes">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <div class="topnav" id="myTopnav">
+            <a href="index.php"><img src ="img/orbitaicon.png"></a>
+            <nav class = "menu_opcoes">
             <ul>
                 <li><a href="#">Sua conta</a></li>
                 <li><a href="#">Portifolio</a></li>
                 <li><a href="#">Contato</a></li>
-                <li><a href="#">Sobre nos</li>
+                <li><a href="#">Sobre nos</a></li>
                 <li><a href="#">Ajuda</a></li>
+                <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    <i class="fa fa-bars"></i>
+                     </a>
                 </ul>
             </nav>
         </div>
-        
-   
+        </div>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <div class="topnav" id="myTopnav">
+  <a href="#home" class="active">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+     <i class="fa fa-bars"></i>
+    </a>
+    </div>   -->
+
     </header>
     
