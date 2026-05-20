a70f9080ca7f2bc9f4ce675643782c8c1ff89e09
odysseyboy <ruanmachadodonati9@gmail.com>
2026-05-20 19:16:48 -0300

    mensagem de commit

--- index.php
+++ index.php
@@ -1,7 +1,31 @@
 <?php 
 include 'inc/header.inc.php';
 ?>
-
+    <link rel="stylesheet" href="css/style.css">
+    <section class="busca">
+        <h2>Buscar Startup</h2>
+        <from>
+            <input type="search" name="busca"placeholder="Digita algo ai">
+            <button>Busca</button>
+        </from>
+    </section>
+    <section>
+        <h2>repositorios</h2>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <div class="topnav" id="myTopnav">
+            <nav class="menu_start">
+                <ul>
+                    <li><a href="https://www.youtube.com/?reload=9">Concluidos</a></li>
+                    <li><a href="#">iniciados</a></li>
+                    <li><a href="#">Confirmados</a></li>
+                    <li><a href="#">Cancelados</a></li>
+                    <li><a href="#">Evidencia</a></li>
+                    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+                    
+                </ul>
+             </nav>
+        </div>
+    </section>
 
 
 <?php 
